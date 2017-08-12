52f403da3de1a4994c99afc8510ac28ce1ca9104
hly9624 <30918122+hly9624@users.noreply.github.com>
2017-08-12 09:59:59 +0800

    deletle

--- file/lianxi1.py
+++
@@ -1,9 +0,0 @@
-import os
-
-def sj(rootDir,outPutFile):
-    fw = open(outPutFile,'w')
-
-    for dirName in os.listdir(rootDir):
-        if os.path.isdir(os.path.join(rootDir,dirName)):
-            print 'process in dir:%s'%dirName
-            for 
